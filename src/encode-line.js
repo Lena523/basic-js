701048f51524f4a6abeba0290dd09a4d6b4c83c8
Elena Khadasevich <camatsu90@mail.ru>
2025-01-04 04:15:05 +0300

    feat: add encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+function encodeLine(str) {
+  if(str.length === 0){
+   return '';
+  }
+  let count = 1;
+  let encodedStr = [];
+  let tempArr = '';
+  for(let i = 0; i < str.length; i += 1){
+    while(str[i] === str[i + 1]){
+     count += 1;
+     i += 1;
+    }
+    if(count > 1){
+       encodedStr.push(count);
+       encodedStr.push(str[i])
+       count = 1;
+    }
+    else{
+      encodedStr.push(str[i]);
+    }
+  }
+  return tempArr = encodedStr.join(''); 
+ }
 
 module.exports = {
   encodeLine
